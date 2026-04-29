5ec41de0112626d0d319fa471f945ec775194c6a
Parmeet Kumar <parmeetkumarg90.com>
2026-04-29 22:48:54 +0530

    work on file upload

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,18 +1,29 @@
 import { Module } from "@nestjs/common";
+import { ConfigModule, ConfigService } from "@nestjs/config";
+import { RouterModule } from "@nestjs/core";
+import { CqrsModule } from "@nestjs/cqrs";
+import { MulterModule } from "@nestjs/platform-express";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
-import { ConfigModule } from "@nestjs/config";
-import { DatabaseModule } from "./infrastructure/database/database";
-import { CqrsModule } from "@nestjs/cqrs";
 import { AudioModule } from "./feature/audio/audio.module";
+import { UploadModule } from "./feature/upload/upload.module";
 import { UserModule } from "./feature/user/user.module";
-import { RouterModule } from "@nestjs/core";
+import { DatabaseModule } from "./infrastructure/database/database";
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     DatabaseModule,
     CqrsModule.forRoot({}),
+    MulterModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        dest: configService.get("LOCAL_UPLOAD_PATH") || "./uploads",
+        preservePath: true,
+      }),
+      inject: [ConfigService],
+    }),
+    UploadModule,
     AudioModule,
     UserModule,
     RouterModule.register([
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,29 @@
+import { ValidationPipe } from "@nestjs/common";
 import { NestFactory } from "@nestjs/core";
+import { urlencoded } from "express";
 import { AppModule } from "./app.module";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({});
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      forbidNonWhitelisted: true, // throws error on unknown values
+      forbidUnknownValues: true, // throws error on unknown request body
+      transform: true, // transform object values or type conversion where possible
+      whitelist: true, // remove unknown values
+    }),
+  );
+
+  app.use(
+    urlencoded({
+      limit: "50mb",
+      extended: true, // allow nested query if true
+    }),
+  );
+
   await app.listen(process.env.APP_PORT ?? 8080);
 }
 bootstrap();
